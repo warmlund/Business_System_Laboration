72e82276dab438068d50795844fb0e04d645541e
warmlund <emeliewarmlund@gmail.com>
2024-04-14 19:46:55 +0200

    Setup CSV link Classes for different types of products

---
+++ CheckoutViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Business_System_Laboration_4
+{
+    public class CustomerViewModel : ViewModelBase, INotifyPropertyChanged
+    {
+
+        private int _totalPrice;
+        private ObservableCollection<Product> _cartItems;
+        public ObservableCollection<Product> CartItems { get { return _cartItems; } set { _cartItems = value; OnPropertyChanged(nameof(CartItems)); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public CustomerViewModel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Checkout()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFromCart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddToCart()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ StockViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business_System_Laboration_4
+{
+    public class StaffViewModel : ViewModelBase
+    {
+        public StaffViewModel()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddProduct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveProduct()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddWholeSaleDelivery()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
