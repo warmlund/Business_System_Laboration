67ddcbbffbcf872878159d3a7bae70d2cbfc5771
warmlund <emeliewarmlund@gmail.com>
2024-05-07 23:12:27 +0200

    added feature for syncing product information with external api

--- apiClasses/ApiHandler.cs
+++ apiClasses/ApiHandler.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Xml;
 
 namespace Business_System_Laboration_4
 {
-    internal class ApiHandler
+    public static class ApiHandler
     {
+        public static async Task<string> SyncWithWebAPI()
+        {
+            string responseData = string.Empty;
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://hex.cse.kau.se/~jonavest/csharp-api/");
+                response.EnsureSuccessStatusCode();
+                
+                if(response.IsSuccessStatusCode)
+                {
+                   responseData = await response.Content.ReadAsStringAsync();
+                }
+
+                else
+                {
+                    MessageBox.Show($"Serverfel:{response.StatusCode}", "Fel");
+                }
+            }
+
+            return responseData;
+        }
+
     }
 }
