1e2aa8659116958075de1d9c9ae4c128d60a103e
warmlund <emeliewarmlund@gmail.com>
2024-05-07 21:36:36 +0200

    Moved file to correct folder

---
+++ apiClasses/ApiHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_System_Laboration_4
+{
+    internal class ApiHandler
+    {
+    }
+}
